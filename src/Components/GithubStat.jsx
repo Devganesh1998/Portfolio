22c8361f2886ca624244bb245e74bfd24380c334
devganesh1998 <devganesh91@gmail.com>
2020-10-21 19:13:31 +0530

    styles moved

--- src/Components/GithubStat.jsx
+++ src/Components/GithubStat.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import GitHubCalander from "react-github-calendar";
-import styles from "./portfolio.module.css";
+import styles from "../styles/portfolio.module.css";
 
-export default (props) => {
-  const [isVisible, setVisible] = React.useState(true);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      console.log(entries);
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-    return () => observer.unobserve(domRef.current);
-  }, []);
+const GitHubStats = (props) => {
+	const [isVisible, setVisible] = React.useState(true);
+	const domRef = React.useRef();
+	React.useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			console.log(entries);
+			entries.forEach((entry) => setVisible(entry.isIntersecting));
+		});
+		observer.observe(domRef.current);
+		return () => observer.unobserve(domRef.current);
+	}, []);
 
-  return (
-    <div
-      className={`${styles.githubStats} ${isVisible ? styles.activePage : ""}`}
-      ref={domRef}
-    >
-      <GitHubCalander username="Devganesh1998"></GitHubCalander>;
-    </div>
-  );
+	return (
+		<div
+			className={`${styles.githubStats} ${
+				isVisible ? styles.activePage : ""
+			}`}
+			ref={domRef}
+		>
+			<GitHubCalander username="Devganesh1998"></GitHubCalander>;
+		</div>
+	);
 };
+
+export default GitHubStats;
