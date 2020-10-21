2675a74bb1c3ad644bb6cfcb649982e4d575630e
devganesh1998 <devganesh91@gmail.com>
2020-10-21 18:39:31 +0530

    icon source update

--- src/Data/IconSource.js
+++ src/Data/IconSource.js
@@ -1,36 +1,18 @@
-const html = "https://devicons.github.io/devicon/devicon.git/icons/html5/html5-original-wordmark.svg";
-const css = "https://devicons.github.io/devicon/devicon.git/icons/css3/css3-original-wordmark.svg";
-const bootstrap = "https://devicons.github.io/devicon/devicon.git/icons/bootstrap/bootstrap-plain-wordmark.svg";
-const react = "https://devicons.github.io/devicon/devicon.git/icons/react/react-original-wordmark.svg";
-const redux = "https://devicons.github.io/devicon/devicon.git/icons/redux/redux-original.svg";
-const mysql = "https://devicons.github.io/devicon/devicon.git/icons/mysql/mysql-original-wordmark.svg";
-const mongodb = "https://devicons.github.io/devicon/devicon.git/icons/mongodb/mongodb-original-wordmark.svg";
-const js = "https://devicons.github.io/devicon/devicon.git/icons/javascript/javascript-original.svg";
-const git = "https://devicons.github.io/devicon/devicon.git/icons/git/git-original-wordmark.svg";
-const node = "icons/nodejs.svg";
-const flask =
-  "https://i7.pngguru.com/preview/509/951/613/flask-by-example-web-framework-python-bottle-bottle.jpg";
-const jquery =
-  "https://devicons.github.io/devicon/devicon.git/icons/jquery/jquery-original-wordmark.svg";
-const antd =
-  "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg";
-const redis = "https://cdn.iconscout.com/icon/free/png-512/redis-5-1175104.png";
-const aws = "https://a0.awsstatic.com/libra-css/images/site/touch-icon-iphone-114-smile.png"
-
-export default {
-  html,
-  git,
-  css,
-  bootstrap,
-  react,
-  redux,
-  mysql,
-  mongodb,
-  js,
-  node,
-  flask,
-  jquery,
-  antd,
-  redis, 
-  aws
-};
+export const nodejs = "/icons/nodejs.svg";
+export const aws = "/icons/aws-icon.png"
+export const postgreSQL = "/icons/postgre-sql-icon.svg"
+export const react = "/icons/react-icon.svg"
+export const kafka = "/icons/kafka-icon.svg"
+export const nextjs = "/icons/nextjs-icon.svg"
+export const redis = "/icons/redis-icon.svg"
+export const flask = "/icons/flask-icon.svg"
+export const antd = "/icons/antd-icon.svg"
+export const jquery = "/icons/jquery-icon.svg"
+export const bootstrap = "/icons/bootstrap-icon.svg"
+export const redux = "/icons/redux-icon.svg"
+export const html = "/icons/html-icon.svg"
+export const js = "/icons/js-icon.svg"
+export const css = "/icons/css-icon.svg"
+export const webpack = "/icons/webpack-icon.svg"
+export const logstash = "/icons/logstash-icon.svg"
+export const elasticSearch = "/icons/elasticsearch-icon.svg"
