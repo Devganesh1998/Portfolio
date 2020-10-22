0b8330ac6b865e8a917192acd172f8343afd3ed7
devganesh1998 <devganesh91@gmail.com>
2020-10-22 20:34:31 +0530

    card data alerted

--- src/Data/CardData.js
+++ src/Data/CardData.js
@@ -1,85 +1,91 @@
-import IconData from "./IconSource";
-
-const { html, css, js, jquery, bootstrap, react, redux, node, mysql, redis, antd, aws, flask } = IconData;
+import {
+	aws,
+	elasticSearch,
+	kafka,
+	logstash,
+	nextjs,
+	nodejs,
+	postgreSQL,
+	react,
+	redis,
+	webpack,
+	antd,
+	flask,
+	redux,
+	css,
+	html,
+	js,
+	bootstrap,
+	jquery,
+} from "./IconSource";
 
 export const cardData = {
-  cardData: [
-    {
-      cardImg: "VrboClone.png",
-      cardHead: "Vrbo Clone",
-      cardBody:
-        "Vrbo Clone is the clone of website Vrbo offering rental bookings for housing properties. Uses Redis and cookies for Session management. Integrated GoogleMap, Razorpay",
-      techStack: [node, redis, mysql, aws, react, redux ],
-      links: {
-        github: "https://github.com/pandayzyx/VrboClone",
-        demo: "https://vrboclone.devganesh.tech",
-      },
-    },
-    {
-      cardImg: "twitterclone.png",
-      cardHead: "Twitter Clone",
-      cardBody:
-        "Twitter Clone is the custom clone version of website Twitter where users can view tweets who they are following and follow / unfollow other users and like their tweets. Uses Redis and cookies for Session management.",
-      techStack: [flask, redis, mysql, aws, react, redux, antd ],
-      links: {
-        github: "https://github.com/Devganesh1998/twitterClone",
-        demo: "https://twitterclone.devganesh.tech",
-      },
-    },
-    {
-      cardImg: "VehicleRenting.png",
-      cardHead: "Vehicle Renting",
-      cardBody:
-        "A Site used to rent cars and bikes and also has Admin functionalities such as adding Vehicle and updating Vehicles. This site has user data management, ie Login, Register, Booking..",
-      techStack: [react, redux, antd, js, css, html],
-      links: {
-        github: "https://github.com/Devganesh1998/VehicleRenting",
-        demo: "https://vehicle-renting.netlify.app/",
-      },
-    },
-    {
-      cardImg: "MovieDatabase.png",
-      cardHead: "Movie Finder",
-      cardBody:
-        "A Site used to find and surf through Movies and Tv-Series and to fetch the details of plot, reviews and much more about the Movies or Tv-Series.",
-      techStack: [html, css, js, jquery, bootstrap],
-      links: {
-        github: "https://github.com/Devganesh1998/Movie-database",
-        demo: "https://dazzling-lalande-7da0f6.netlify.app/",
-      },
-    },
-    {
-      cardImg: "Dinosaurs.png",
-      cardHead: "Dinosaur's Info",
-      cardBody:
-        "A simple multi-page site which contains details of dinosaurs such as their extinction, types and history. Created using plain HTML and CSS.",
-      techStack: [html, css],
-      links: {
-        github: "https://github.com/Devganesh1998/masai-sprint-1",
-        demo: "https://zealous-hopper-460016.netlify.app/",
-      },
-    },
-    {
-      cardImg: "Tic-Tac-Toe.png",
-      cardHead: "Tic-Tac-Toe",
-      cardBody:
-        "A Javascript based two-player game called as Tic-Tac-Toe. This classic game is build using plain HTML, CSS, Javascript.",
-      techStack: [html, css, js],
-      links: {
-        github: "https://github.com/Devganesh1998/Tic-Tac-Toe",
-        demo: "https://cocky-swartz-8de7d5.netlify.app/",
-      },
-    },
-    // {
-    //   cardImg: "BillGenerator.png",
-    //   cardHead: "BillGenerator",
-    //   cardBody:
-    //     "A simple bill generator web-page created using plain HTML, CSS, Javascript. Have options to Tip and applys discount based on type of day.",
-    //   techStack: [html, css, js],
-    //   links: {
-    //     github: "https://github.com/Devganesh1998/masai-sprint-2",
-    //     demo: "https://flamboyant-gates-e03f3b.netlify.app/",
-    //   },
-    // },
-  ],
+	cardData: [
+		{
+			cardImg: "VrboClone.png",
+			cardHead: "Vrbo Clone",
+			cardBody:
+				"Vrbo Clone is the clone of website Vrbo offering rental bookings for housing properties. Uses Redis and cookies for Session management. Integrated GoogleMap, Razorpay",
+			techStack: [nodejs, redis, postgreSQL, aws, react, redux],
+			links: {
+				github: "https://github.com/pandayzyx/VrboClone",
+				demo: "https://vrboclone.devganesh.tech",
+			},
+		},
+		{
+			cardImg: "twitterclone.png",
+			cardHead: "Twitter Clone",
+			cardBody:
+				"Twitter Clone is the custom clone version of website Twitter where users can view tweets who they are following and follow / unfollow other users and like their tweets. Uses Redis and cookies for Session management.",
+			techStack: [flask, redis, postgreSQL, aws, react, redux, antd],
+			links: {
+				github: "https://github.com/Devganesh1998/twitterClone",
+				demo: "https://twitterclone.devganesh.tech",
+			},
+		},
+		{
+			cardImg: "VehicleRenting.png",
+			cardHead: "Vehicle Renting",
+			cardBody:
+				"A Site used to rent cars and bikes and also has Admin functionalities such as adding Vehicle and updating Vehicles. This site has user data management, ie Login, Register, Booking..",
+			techStack: [react, redux, antd, js, css, html],
+			links: {
+				github: "https://github.com/Devganesh1998/VehicleRenting",
+				demo: "https://vehicle-renting.netlify.app/",
+			},
+		},
+		{
+			cardImg: "MovieDatabase.png",
+			cardHead: "Movie Finder",
+			cardBody:
+				"A Site used to find and surf through Movies and Tv-Series and to fetch the details of plot, reviews and much more about the Movies or Tv-Series.",
+			techStack: [html, css, js, jquery, bootstrap],
+			links: {
+				github: "https://github.com/Devganesh1998/Movie-database",
+				demo: "https://dazzling-lalande-7da0f6.netlify.app/",
+			},
+		},
+		{
+			cardImg: "Dinosaurs.png",
+			cardHead: "Dinosaur's Info",
+			cardBody:
+				"A simple multi-page site which contains details of dinosaurs such as their extinction, types and history. Created using plain HTML and CSS.",
+			techStack: [html, css],
+			links: {
+				github: "https://github.com/Devganesh1998/masai-sprint-1",
+				demo: "https://zealous-hopper-460016.netlify.app/",
+			},
+		},
+		{
+			cardImg: "Tic-Tac-Toe.png",
+			cardHead: "Tic-Tac-Toe",
+			cardBody:
+				"A Javascript based two-player game called as Tic-Tac-Toe. This classic game is build using plain HTML, CSS, Javascript.",
+			techStack: [html, css, js],
+			links: {
+				github: "https://github.com/Devganesh1998/Tic-Tac-Toe",
+				demo: "https://cocky-swartz-8de7d5.netlify.app/",
+			},
+		},
+	],
 };
