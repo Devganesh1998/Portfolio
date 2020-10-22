9be97d744b11b4583e84967200fd3d3a5a5c0059
devganesh1998 <devganesh91@gmail.com>
2020-10-22 20:39:31 +0530

    techstack img updated

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,40 +1,55 @@
 import React from "react";
-import styles from "./portfolio.module.css";
-import IconSourceData from "../Data/IconSource"
+import styles from "../styles/portfolio.module.css";
+import {
+	aws,
+	elasticSearch,
+	kafka,
+	logstash,
+	nextjs,
+	nodejs,
+	postgreSQL,
+	react,
+	webpack,
+	redis,
+} from "../Data/IconSource";
 
-export default () => {
-  return (
-    <div className={styles.techStack}>
-      <h2
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="600"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-once="false"
-      >
-        Tech Stack
-      </h2>
-      <div
-        className={styles.stackImgs}
-        data-aos="fade-up"
-        data-aos-offset="220"
-        data-aos-delay="800"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-once="false"
-      >
-        <img src={IconSourceData.html} alt="HTML5"/>
-        <img src="https://devicons.github.io/devicon/devicon.git/icons/git/git-original-wordmark.svg" alt="Git"/>
-        <img src={IconSourceData.css} alt="Css"/>
-        <img src="icons/nodejs.svg" alt="NodeJs"/>
-        <img src={IconSourceData.js} alt="Javascript"/>
-        <img src={IconSourceData.react} alt="React"/>
-        <img src={IconSourceData.mongodb} alt="MongoDB"/>
-        <img src="https://i7.pngguru.com/preview/509/951/613/flask-by-example-web-framework-python-bottle-bottle.jpg" alt="Flask"/>
-        <img src="https://a0.awsstatic.com/libra-css/images/site/touch-icon-iphone-114-smile.png" alt="AWS"/>
-        <img src={IconSourceData.mysql} alt="MySQL"/>
-      </div>
-    </div>
-  );
-};
+const TechStack = () => (
+	<div className={styles.techStack}>
+		<h2
+			data-aos="fade-up"
+			data-aos-offset="200"
+			data-aos-delay="600"
+			data-aos-duration="1000"
+			data-aos-easing="ease-in-out"
+			data-aos-once="false"
+		>
+			Tech Stack
+		</h2>
+		<div
+			className={styles.stackImgs}
+			data-aos="fade-up"
+			data-aos-offset="220"
+			data-aos-delay="800"
+			data-aos-duration="1000"
+			data-aos-easing="ease-in-out"
+			data-aos-once="false"
+		>
+			<img title="Nodejs" src={nodejs} alt="NodeJs" />
+			<img title="Kafka" src={kafka} alt="kafka" />
+			<img
+				title="ElasticSearch"
+				src={elasticSearch}
+				alt="elasticSearch"
+			/>
+			<img title="Logstash" src={logstash} alt="logstash" />
+			<img title="PostgreSQL" src={postgreSQL} alt="SQL" />
+			<img title="Redis" src={redis} alt="redis" />
+			<img title="Nextjs" src={nextjs} alt="nextjs" />
+			<img title="Webpack" src={webpack} alt="webpack" />
+			<img title="React" src={react} alt="React" />
+			<img title="AWS" src={aws} alt="AWS" />
+		</div>
+	</div>
+);
+
+export default TechStack;
