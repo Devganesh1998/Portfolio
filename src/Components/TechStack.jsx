1db8e4aea9e3a4ea788b4a695e16ed2a69df073b
devganesh1998 <devganesh91@gmail.com>
2020-10-23 20:13:31 +0530

    demo open metric added

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from 'react-ga';
 import styles from "../styles/portfolio.module.css";
 import {
 	aws,
@@ -13,43 +14,51 @@ import {
 	redis,
 } from "../Data/IconSource";
 
-const TechStack = () => (
-	<div className={styles.techStack}>
-		<h2
-			data-aos="fade-up"
-			data-aos-offset="200"
-			data-aos-delay="100"
-			data-aos-duration="1000"
-			data-aos-easing="ease-in-out"
-			data-aos-once="false"
-		>
-			Tech Stack
-		</h2>
-		<div
-			className={styles.stackImgs}
-			data-aos="fade-up"
-			data-aos-offset="220"
-			data-aos-delay="150"
-			data-aos-duration="1000"
-			data-aos-easing="ease-in-out"
-			data-aos-once="false"
-		>
-			<img title="Nodejs" src={nodejs} alt="NodeJs" />
-			<img title="Kafka" src={kafka} alt="kafka" />
-			<img
-				title="ElasticSearch"
-				src={elasticSearch}
-				alt="elasticSearch"
-			/>
-			<img title="Logstash" src={logstash} alt="logstash" />
-			<img title="PostgreSQL" src={postgreSQL} alt="SQL" />
-			<img title="Redis" src={redis} alt="redis" />
-			<img title="Nextjs" src={nextjs} alt="nextjs" />
-			<img title="Webpack" src={webpack} alt="webpack" />
-			<img title="React" src={react} alt="React" />
-			<img title="AWS" src={aws} alt="AWS" />
+const TechStack = () => {
+	useEffect(() => {
+		ReactGA.event({
+			category: "Section visited",
+			action: "TechStack section",
+		});
+	}, []);
+	return (
+		<div className={styles.techStack}>
+			<h2
+				data-aos="fade-up"
+				data-aos-offset="200"
+				data-aos-delay="100"
+				data-aos-duration="1000"
+				data-aos-easing="ease-in-out"
+				data-aos-once="false"
+			>
+				Tech Stack
+			</h2>
+			<div
+				className={styles.stackImgs}
+				data-aos="fade-up"
+				data-aos-offset="220"
+				data-aos-delay="150"
+				data-aos-duration="1000"
+				data-aos-easing="ease-in-out"
+				data-aos-once="false"
+			>
+				<img title="Nodejs" src={nodejs} alt="NodeJs" />
+				<img title="Kafka" src={kafka} alt="kafka" />
+				<img
+					title="ElasticSearch"
+					src={elasticSearch}
+					alt="elasticSearch"
+				/>
+				<img title="Logstash" src={logstash} alt="logstash" />
+				<img title="PostgreSQL" src={postgreSQL} alt="SQL" />
+				<img title="Redis" src={redis} alt="redis" />
+				<img title="Nextjs" src={nextjs} alt="nextjs" />
+				<img title="Webpack" src={webpack} alt="webpack" />
+				<img title="React" src={react} alt="React" />
+				<img title="AWS" src={aws} alt="AWS" />
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default TechStack;
